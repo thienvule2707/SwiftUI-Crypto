6fb153641c8cb1e9851005afb6db8067dd964cb3
Dora-Tran <dora.tran@watasolutions.com>
2023-03-14 09:37:05 +0700

    Initial Commit

---
+++ SwiftUI-Crypto/SwiftUI_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoApp.swift
+//  SwiftUI-Crypto
+//
+//  Created by Thien Vu Le on Mar/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-CryptoUITests/SwiftUI_CryptoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_CryptoUITestsLaunchTests.swift
+//  SwiftUI-CryptoUITests
+//
+//  Created by Thien Vu Le on Mar/14/23.
+//
+
+import XCTest
+
+final class SwiftUI_CryptoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
